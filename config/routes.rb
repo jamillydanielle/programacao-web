a4364a1bcee2b73dfd0ef39cdbecf30e0ac18f96
ivanmaxk <ivan.satelite1@gmail.com>
2024-08-13 09:36:50 -0300

    frontend quase pronto

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,19 @@
 Rails.application.routes.draw do
-  
-get '/produtos', to: 'produtos#index'
+  get '/produtos', to: 'produtos#index'
+
+  get '/produtos/new', to: 'produtos#new', as: 'new_produto'
+
+  get '/produtos/:id', to: 'produtos#show', as: 'produto'
+
+  post '/produtos', to: 'produtos#create'
+
+  get '/produtos/:id/edit', to: 'produtos#edit', as: 'edit_produto'
+
+  patch '/produtos/:id', to: 'produtos#update'
+  put '/produtos/:id', to: 'produtos#update'
+
+  delete '/produtos/:id', to: 'produtos#destroy', as: 'destroy_produto'
 
 
-get '/produtos/:id', to: 'produtos#show', as: 'produto'
-
-
-get '/produtos/new', to: 'produtos#new', as: 'new_produto'
-
-
-post '/produtos', to: 'produtos#create'
-
-
-get '/produtos/:id/edit', to: 'produtos#edit', as: 'edit_produto'
-
-
-patch '/produtos/:id', to: 'produtos#update'
-put '/produtos/:id', to: 'produtos#update'
-
-
-delete '/produtos/:id', to: 'produtos#destroy'
-
-root to: 'produtos#index'
-  
+  root to: 'produtos#index'
 end
